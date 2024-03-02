ae6803a46358114a9ec488f75979546fe53c2355
CannBayazit <bayazitdogancan@gmail.com>
2024-03-02 19:42:53 +0300

    label düzenlendi

--- tabs/saldir_tab.py
+++ tabs/saldir_tab.py
@@ -11,12 +11,16 @@ def create_saldiri_tab(tab_control):
 
     label_altin = tk.Label(saldiri_tab, text="Altın Miktarı")
     label_altin.grid(row=0, column=0, padx=10, pady=10)
+    label_altin_etry = tk.Entry(saldiri_tab)
+    label_altin_etry.grid(row=0, column=1, padx=10, pady=10)
 
     label_iksir = tk.Label(saldiri_tab, text="İksir Miktarı")
-    label_iksir.grid(row=1, column=0, padx=10, pady=5)     
+    label_iksir.grid(row=1, column=0, padx=10, pady=5)  
+    label_iksir_entry = tk.Entry(saldiri_tab)
+    label_iksir_entry.grid(row=1, column=1, padx=10, pady=10)   
     
     ganimet_ara = tk.Button(saldiri_tab, text="Ganimet Ara")
-    ganimet_ara.place(x=150, y=400)
+    ganimet_ara.place(x=100, y=100)
     ganimet_ara.config(command=button_click)
 
     attack_button = tk.Button(saldiri_tab, text="Saldır")
